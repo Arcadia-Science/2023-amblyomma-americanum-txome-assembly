243fc1e4c735b8136315000e472cb2da8589f20d
taylorreiter <taylorreiter@gmail.com>
2023-08-18 16:24:31 +0000

    add py script for selecting orthogroup highest score contig

---
+++ sandbox/try_orthofuser/scripts/get_contig_name_w_highest_transrate_score_for_each_orthogroup.py
@@ -0,0 +1,50 @@
+import argparse
+from collections import defaultdict
+
+def read_orthogroups(filepath):
+    orthogroups = defaultdict(list)
+
+    with open(filepath, 'r') as file:
+        for line in file:
+            orthogroup, contig_names = line.strip().split(": ")
+            for contig in contig_names.split():
+                orthogroups[orthogroup].append(contig)
+
+    return orthogroups
+
+def process_contigs_csv(filepath, orthogroups):
+    contig_scores = {}
+    contig_coverage = {}
+
+    with open(filepath, 'r') as file:
+        header = file.readline().strip().split(',')
+        score_idx = header.index("score")
+        coverage_idx = header.index("coverage")
+
+        for line in file:
+            parts = line.strip().split(',')
+            contig_scores[parts[0]] = float(parts[score_idx])
+            contig_coverage[parts[0]] = float(parts[coverage_idx])
+
+    best_contigs = []
+
+    for contigs in orthogroups.values():
+        contigs.sort(key=lambda x: (contig_scores.get(x, 0), contig_coverage.get(x, 0)), reverse=True)
+        best_contigs.append(contigs[0])
+
+    return best_contigs
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process orthogroups and contigs.")
+    parser.add_argument("orthogroups_path", help="Path to the orthogroups file")
+    parser.add_argument("contigs_csv_path", help="Path to the contigs.csv file")
+    parser.add_argument("output_path", help="Path for the output file")
+    args = parser.parse_args()
+
+    orthogroups = read_orthogroups(args.orthogroups_path)
+    best_contigs = process_contigs_csv(args.contigs_csv_path, orthogroups)
+
+    with open(args.output_path, 'w') as out_file:
+        for contig in best_contigs:
+            out_file.write(f"{contig}\n")
+
